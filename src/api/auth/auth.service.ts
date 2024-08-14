0be9e9a85b57099fa3626b12d12cf674fd19b75f
Spectra.codes by Drix10 <drixisgod@gmail.com>
2024-08-15 03:26:30 +0530

    generated file: `src/api/auth/auth.service.ts`

---
+++ src/api/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../user/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login-user.dto';
+import  as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (user && (await bcrypt.compare(pass, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async register(email: string, password: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = this.userRepository.create({
+      email,
+      password: hashedPassword,
+    });
+    return this.userRepository.save(user);
+  }
+}
