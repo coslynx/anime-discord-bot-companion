83be6839f6ee2a987ef0b9a863ad4bccca690bb8
Spectra.codes by Drix10 <drixisgod@gmail.com>
2024-08-15 03:43:05 +0530

    generated file: `src/constants/api.constants.ts`

---
+++ src/constants/api.constants.ts
@@ -0,0 +1,7 @@
+export const ApiConstants = {
+  ANILIST_API_URL: 'https://api.anilist.co',
+  MY_ANIME_LIST_API_URL: 'https://api.myanimelist.net/v2',
+  JIKAN_API_URL: 'https://api.jikan.moe/v4',
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  UPLOADS_DIR: './uploads',
+};
