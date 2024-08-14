5deac96f9abf95827e8291fae3e92c508ec7c69c
Spectra.codes by Drix10 <drixisgod@gmail.com>
2024-08-15 03:38:21 +0530

    generated file: `src/modules/list/list.module.ts`

---
+++ src/modules/list/list.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ListService } from './list.service';
+import { ListController } from './list.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { List } from './entities/list.entity';
+import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([List]),
+    UserModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '3600s' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [ListController],
+  providers: [ListService],
+  exports: [ListService],
+})
+export class ListModule {}
